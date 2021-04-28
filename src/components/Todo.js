f9d97c668acc7849fbe9f939b2591313012080a5
Rafael Lemos <rhplemos@gmail.com>
2021-04-27 21:23:27 -0300

    roteiro 2

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -17,15 +17,17 @@ const Todo = () => {
         <> 
         <form onSubmit = {handleFormSubmit}>
             <input 
-                data-testid=""
+                data-testid="form-field"
                 onChange = {handleInputChange}
                 placeholder = "Type a new task here"
                 typr = "text"
                 value = {task}
             />
-            <button type = "submit">Add new task</button>            
+            <button
+                 data-testid="form-btn"
+                 type = "submit">Add new task</button>            
         </form>
-        <table>
+        <table data-testid="table">
             <thead>
                 <tr>
                     <th>Task</th>
--- src/components/Todo.test.js
+++ src/components/Todo.test.js
@@ -1,16 +1,30 @@
 import React from 'react'
 
-import { render, waitForElement } from '@testing-library/react'
+import { fireEvent, render, waitForElement } from '@testing-library/react'
 
 import Todo from './Todo'
 
 describe ('Tests for Todo component', () => {
     it('Should add new task when form has been submitted', async () => {
-        const { getByTestId} = render (<Todo/>)//renderizar o componente
-        const fieldNode = await  waitForElement(//buscar o input
+        const { getByTestId} = render (<Todo/>) //renderizar o componente
+        const fieldNode = await  waitForElement( //buscar o input
             () => getByTestId('form-field')
         )
-        console.log(fieldNode)
+        const newTask = 'testing'
+        fireEvent.change( //digitar no input
+            fieldNode , 
+            {target: {value: 'newTask'}}
+        )
+        expect(fieldNode.value).toEqual('newTask')
+
+        const btnNode = await waitForElement( //buscar botão
+            () => getByTestId('form-btn')
+        )
+        fireEvent.click(btnNode) //clicar no botão
+
+        const tableNode = await waitForElement( //buscar tabela
+            () => getByTestId('table')  
+        )
         //renderizar o componente
         //buscar o input
         //digitar no input
